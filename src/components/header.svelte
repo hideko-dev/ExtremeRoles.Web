b8adeaacf43789048d9ef9efdff6a68ef0ccdf13
Hideko <hideko332200@gmail.com>
2023-11-25 23:18:58 +0900

    add logo menu

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,5 +1,24 @@
+<script>
+    import Fa from "svelte-fa";
+    import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+    import Menu from "./menu.svelte";
+    import {menuState, toggleMenu} from "$lib/menu.js";
+    let arrowRotate = 0;
+    $: {
+        if($menuState === false){
+            arrowRotate = 0;
+        } else if($menuState === true){
+            arrowRotate = -180;
+        }
+    }
+</script>
+
 <section>
-    <p class="title">Header temp</p>
+    <div class="logo" on:click={toggleMenu}>
+        <p>ExtremeRoles</p>
+        <div class="icon" style="rotate: {arrowRotate}deg"><Fa icon={faChevronDown} size="14"/></div>
+        <Menu/>
+    </div>
 </section>
 
 <style>
@@ -14,8 +33,20 @@
         justify-content: center;
         font-family: 'Poppins', sans-serif;
     }
-    .title {
+    .icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: all 0.3s;
+    }
+    .logo {
         font-weight: 500;
         font-size: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 8px;
+        cursor: pointer;
+        user-select: none;
     }
 </style>
---
+++ src/components/menu.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { menuState } from "$lib/menu.js";
+    import {faChild, faHatWizard, faSignature} from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
+    let style = "opacity: 0; scale: 0.7";
+    $: {
+        if($menuState === false){
+            style = "opacity: 0; scale: 0.9";
+        } else if($menuState === true){
+            style = "opacity: 1; scale: 1";
+        }
+    }
+    const menus = [
+        {title: "ExtremeHats", sub: "Hats for ExtremeSkins", icon: faHatWizard},
+        {title: "ExtremeSkins", sub: "Skins for ExtremeRoles", icon: faChild},
+        {title: "ExtremeNamePlate", sub: "NamePlate for ExtremeRoles", icon: faSignature},
+    ]
+</script>
+
+<section style={style}>
+    {#each menus as m}
+        <div class="box">
+            <div class="top">
+                <Fa icon={m.icon}/>
+                <p class="title">{m.title}</p>
+            </div>
+            <p class="sub">{m.sub}</p>
+        </div>
+    {/each}
+</section>
+
+<style>
+    .box {
+        font-size: 15px;
+        padding-inline: 10px;
+        padding-block: 5px;
+        transition: all 0.2s;
+        border-radius: 8px;
+    }
+    .box:hover {
+        background: #e5e5e5;
+    }
+    .top {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+    }
+    .sub {
+        font-size: 10px;
+    }
+    section {
+        top: 3.2rem;
+        position: absolute;
+        width: 13rem;
+        height: max-content;
+        background:rgb(244,244,244);
+        box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+        transition: all 0.2s;
+        border-radius: 10px;
+        transform-origin: top;
+        padding: 5px;
+    }
+</style>
